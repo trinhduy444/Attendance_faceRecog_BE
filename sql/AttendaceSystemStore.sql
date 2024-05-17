bc8016842b30ccdb9d0db6c4026b567d0adb9090
LiaCyrus <118715767+LiaCyrus@users.noreply.github.com>
2024-05-17 23:06:02 +0700

    Update pagination store return page info
    Add select statement return info of current page

--- sql/AttendaceSystemStore.sql
+++ sql/AttendaceSystemStore.sql
@@ -1,4 +1,4 @@
-ALTER PROCEDURE TableLoadingPagination
+CREATE PROCEDURE TableLoadingPagination
 	@Table VARCHAR(32),
 	@OtherJoins VARCHAR(4000) = '',
 	@OtherFields VARCHAR(4000) = '',
@@ -9,22 +9,41 @@ ALTER PROCEDURE TableLoadingPagination
 AS
 BEGIN
 	DECLARE @q NVARCHAR(MAX), @Offset INT
-	SELECT @Offset = (@PageNumber - 1) * @RowPerPage, @Key = @Search
-	IF @Key = '' SET @Key = '(1=1)'
+	SELECT @Offset = (@PageNumber - 1) * @RowPerPage
+
+	IF @OtherFields <> '' SET @OtherFields = ', ' + @OtherFields
+	-- Default value
+	IF @Order = '' SET @Order = '1'
+	IF @Search = '' SET @Search = '(1=1)'
+	IF @PageNumber <= 0 SET @PageNumber = 1
+	IF @RowPerPage <= 0 SET @RowPerPage = 50
 
 	SET @q = '
-		select a.*
-			from (select top @@RowPerPage a.* @@OtherFields from @@table a) a
-				@@OtherJoins
+		-- Page data
+		select a.* @@OtherFields
+			into #v from @@table a @@OtherJoins
 			where @@Search
-		order by @@Order
+			order by @@Order
+		offset @@Offset rows fetch next @@RowPerPage rows only
+	
+		select * from #v order by @@Order
+
+		-- Page info
+		declare @TotalPage int, @PageTotalRow int, @TotalRow int
+		
+		select @PageTotalRow = count(1) from #v
+		select @TotalRow = count(1) from @@table
+		select @TotalPage = ceiling((1.0 * @TotalRow) / @@RowPerPage)
+
+		select iif(@TotalPage = 0, 1, @TotalPage) as total_page, @PageTotalRow as page_total_row, @TotalRow as total_row
+		drop table #v
 	'
+
+	-- Replace params
 	SET @q = REPLACE(@q, '@@table', @Table)
 	SET @q = REPLACE(REPLACE(@q, '@@OtherJoins', @OtherJoins), '@@OtherFields', @OtherFields)
-	SET @q = REPLACE(REPLACE(@q, '@@Order', @Order), '@@Searcg', @Search)
+	SET @q = REPLACE(REPLACE(@q, '@@Order', @Order), '@@Search', @Search)
 	SET @q = REPLACE(REPLACE(@q, '@@RowPerPage', RTRIM(@RowPerPage)), '@@Offset', RTRIM(@Offset))
-	PRINT(@q)
+	PRINT @q
+	EXEC (@q)
 END
-GO
-
-EXEC TableLoadingPagination 'Sysuser'
