d112935555be60777b26aa181e8bd3c352a82166
LiaCyrus <118715767+LiaCyrus@users.noreply.github.com>
2024-07-30 02:19:10 +0700

    Update SQL File
    - Add more seed values
    - Clean up init code
    - Add seperate sql for version update

--- sql/AttendanceSystem.sql
+++ sql/AttendanceSystem.sql
@@ -7,6 +7,18 @@ USE AttendanceSystem
 GO
 
 -- Create Table
+CREATE TABLE Faculty
+(
+	[faculty_id] INT NOT NULL,
+	[faculty_name] NVARCHAR(256) NULL,
+	[creator_id]  INT NULL,
+	[updater_id]  INT NULL,
+	[create_time] DATETIME NULL,
+	[update_time] DATETIME NULL,
+)
+ALTER TABLE Faculty ADD CONSTRAINT PK_Faculty PRIMARY KEY CLUSTERED(faculty_id) ON [PRIMARY]
+GO
+
 CREATE TABLE SysRole
 (
 	[role_id] TINYINT IDENTITY(1, 1) NOT NULL,
@@ -28,18 +40,21 @@ CREATE TABLE SysUser
 	[password] VARCHAR(128) NULL,
 	[nickname] NVARCHAR(256) NULL,
 	[phone] VARCHAR(32) NULL,
+	[gender] BIT NULL,
 	[avatar_path] VARCHAR(128) NULL,
 	[face_image_path] VARCHAR(128) NULL,
+	[course_year] INT NULL,
+	[faculty_id] INT NULL,
 	[role_id] TINYINT,
 	[status] BIT NULL,
 	[creator_id] INT NULL,
 	[updater_id] INT NULL,
 	[create_time] DATETIME NULL,
 	[update_time] DATETIME NULL
-)
-
+)	
 ALTER TABLE SysUser ADD CONSTRAINT PK_SysUser PRIMARY KEY CLUSTERED(user_id) ON [PRIMARY]
-ALTER TABLE SysUser ADD CONSTRAINT FK_SysUserRole FOREIGN KEY(role_id) REFERENCES SysRole(role_id)
+ALTER TABLE SysUser ADD CONSTRAINT FK_SysUser_Role FOREIGN KEY(role_id) REFERENCES SysRole(role_id)
+ALTER TABLE SysUser ADD CONSTRAINT FK_SysUser_Faculty FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id);
 GO
 
 CREATE TABLE Course
@@ -48,6 +63,7 @@ CREATE TABLE Course
 	[course_name] NVARCHAR(256) NULL,
 	[credit] INT NULL,
 	[description] NVARCHAR(512) NULL,
+	[faculty_id] INT NULL,
 	[status] BIT NULL,
 	[creator_id] INT NULL,
 	[updater_id] INT NULL,
@@ -55,6 +71,7 @@ CREATE TABLE Course
 	[update_time] DATETIME NULL
 )
 ALTER TABLE Course ADD CONSTRAINT PK_Course PRIMARY KEY CLUSTERED(course_code) ON [PRIMARY]
+ALTER TABLE Course ADD CONSTRAINT FK_Course_Faculty FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id);
 GO
 
 CREATE TABLE Classroom
@@ -87,12 +104,29 @@ CREATE TABLE Shift
 ALTER TABLE Shift ADD CONSTRAINT PK_Shift PRIMARY KEY CLUSTERED(shift_code) ON [PRIMARY]
 GO
 
+CREATE TABLE ClassRoomShift
+(
+	[classroomshift_id] INT IDENTITY(1, 1) NOT NULL,
+	[classroom_code] VARCHAR(32),
+	[shift_code] VARCHAR(32),
+	[status] BIT DEFAULT 0,
+	[creator_id] INT NULL,
+	[updater_id] INT NULL,
+	[create_time] DATETIME NULL,
+	[update_time] DATETIME NULL
+)
+ALTER TABLE ClassRoomShift ADD CONSTRAINT PK_ClassRoomShift PRIMARY KEY CLUSTERED(classroomshift_id) ON [PRIMARY]
+ALTER TABLE ClassRoomShift ADD CONSTRAINT FK_ClassRoomShift_Classroom FOREIGN KEY(classroom_code) REFERENCES Classroom(classroom_code)
+ALTER TABLE ClassRoomShift ADD CONSTRAINT FK_ClassRoomShift_Shift FOREIGN KEY(shift_code) REFERENCES Shift(shift_code)
+GO
+
 CREATE TABLE CourseGroup
 (
 	[course_group_id] INT IDENTITY(1, 1) NOT NULL,
 	[course_code] VARCHAR(32) NULL,
 	[group_code] VARCHAR(32) NULL,
 	[teacher_id] INT NULL,
+	[classroomshift_id] INT UNIQUE,
 	[total_student_qty] INT NULL,
 	[status] BIT NULL,
 	[creator_id] INT NULL,
@@ -101,8 +135,9 @@ CREATE TABLE CourseGroup
 	[update_time] DATETIME NULL
 )
 ALTER TABLE CourseGroup ADD CONSTRAINT PK_CourseGroup PRIMARY KEY CLUSTERED(course_group_id) ON [PRIMARY]
-ALTER TABLE CourseGroup ADD CONSTRAINT FK_CourseGroupCourse FOREIGN KEY(course_code) REFERENCES Course(course_code)
-ALTER TABLE CourseGroup ADD CONSTRAINT FK_CourseGroupUserTeacher FOREIGN KEY(teacher_id) REFERENCES SysUser(user_id)
+ALTER TABLE CourseGroup ADD CONSTRAINT FK_CourseGroup_Course FOREIGN KEY(course_code) REFERENCES Course(course_code)
+ALTER TABLE CourseGroup ADD CONSTRAINT FK_CourseGroup_UserTeacher FOREIGN KEY(teacher_id) REFERENCES SysUser(user_id)
+ALTER TABLE CourseGroup ADD CONSTRAINT FK_CourseGroup_ClassRoomShift FOREIGN KEY (classroomshift_id) REFERENCES ClassRoomShift(classroomshift_id)
 GO
 
 CREATE TABLE CourseGroupStudentList
@@ -118,8 +153,8 @@ CREATE TABLE CourseGroupStudentList
 	[update_time] DATETIME NULL
 )
 ALTER TABLE CourseGroupStudentList ADD CONSTRAINT PK_CourseGroupStudentList PRIMARY KEY CLUSTERED(course_group_id, student_id) ON [PRIMARY]
-ALTER TABLE CourseGroupStudentList ADD CONSTRAINT FK_CourseGroupStudentListCourseGroup FOREIGN KEY(course_group_id) REFERENCES CourseGroup(course_group_id)
-ALTER TABLE CourseGroupStudentList ADD CONSTRAINT FK_CourseGroupStudentListUserStudent FOREIGN KEY(student_id) REFERENCES SysUser(user_id)
+ALTER TABLE CourseGroupStudentList ADD CONSTRAINT FK_CourseGroupStudentList_CourseGroup FOREIGN KEY(course_group_id) REFERENCES CourseGroup(course_group_id)
+ALTER TABLE CourseGroupStudentList ADD CONSTRAINT FK_CourseGroupStudentList_UserStudent FOREIGN KEY(student_id) REFERENCES SysUser(user_id)
 GO
 
 CREATE TABLE Schedule
@@ -138,9 +173,9 @@ CREATE TABLE Schedule
 	[update_time] DATETIME NULL
 )
 ALTER TABLE Schedule ADD CONSTRAINT PK_Schedule PRIMARY KEY CLUSTERED(course_group_id, shift_code, classroom_code, date_from, date_to) ON [PRIMARY]
-ALTER TABLE Schedule ADD CONSTRAINT FK_ScheduleCourseGroup FOREIGN KEY(course_group_id) REFERENCES CourseGroup(course_group_id)
-ALTER TABLE Schedule ADD CONSTRAINT FK_ScheduleShift FOREIGN KEY(shift_code) REFERENCES Shift(shift_code)
-ALTER TABLE Schedule ADD CONSTRAINT FK_ScheduleClassroom FOREIGN KEY(classroom_code) REFERENCES Classroom(classroom_code)
+ALTER TABLE Schedule ADD CONSTRAINT FK_Schedule_CourseGroup FOREIGN KEY(course_group_id) REFERENCES CourseGroup(course_group_id)
+ALTER TABLE Schedule ADD CONSTRAINT FK_Schedule_Shift FOREIGN KEY(shift_code) REFERENCES Shift(shift_code)
+ALTER TABLE Schedule ADD CONSTRAINT FK_Schedule_Classroom FOREIGN KEY(classroom_code) REFERENCES Classroom(classroom_code)
 GO
 
 CREATE TABLE AttendanceRawData
@@ -155,16 +190,16 @@ CREATE TABLE AttendanceRawData
 	[create_time] DATETIME NULL
 )
 ALTER TABLE AttendanceRawData ADD CONSTRAINT PK_AttendanceRawData PRIMARY KEY CLUSTERED(student_id, course_group_id, attend_date, attend_type) ON [PRIMARY]
-ALTER TABLE AttendanceRawData ADD CONSTRAINT FK_AttendanceRawDataUserStudent FOREIGN KEY(student_id) REFERENCES SysUser(user_id)
-ALTER TABLE AttendanceRawData ADD CONSTRAINT FK_AttendanceRawDataCourseGroup FOREIGN KEY(course_group_id) REFERENCES CourseGroup(course_group_id)
+ALTER TABLE AttendanceRawData ADD CONSTRAINT FK_AttendanceRawData_UserStudent FOREIGN KEY(student_id) REFERENCES SysUser(user_id)
+ALTER TABLE AttendanceRawData ADD CONSTRAINT FK_AttendanceRawData_CourseGroup FOREIGN KEY(course_group_id) REFERENCES CourseGroup(course_group_id)
 GO
 
 CREATE TABLE Attendance
 (
-	[attendance_id] INT IDENTITY(1, 1) NOT NULL,
 	[student_id] INT NOT NULL,
 	[course_group_id] INT NOT NULL,
 	[attend_date] DATE NOT NULL,
+	[attend_yn] BIT NOT NULL,
 	[enter_time] CHAR(5) NULL,
 	[leave_time] CHAR(5) NULL,
 	[note] NVARCHAR(256) NULL,
@@ -175,8 +210,8 @@ CREATE TABLE Attendance
 	[update_time] DATETIME NULL
 )
 ALTER TABLE Attendance ADD CONSTRAINT PK_Attendance PRIMARY KEY CLUSTERED(student_id, course_group_id, attend_date) ON [PRIMARY]
-ALTER TABLE Attendance ADD CONSTRAINT FK_AttendanceUserStudent FOREIGN KEY(student_id) REFERENCES SysUser(user_id)
-ALTER TABLE Attendance ADD CONSTRAINT FK_AttendanceCourseGroup FOREIGN KEY(course_group_id) REFERENCES CourseGroup(course_group_id)
+ALTER TABLE Attendance ADD CONSTRAINT FK_Attendance_UserStudent FOREIGN KEY(student_id) REFERENCES SysUser(user_id)
+ALTER TABLE Attendance ADD CONSTRAINT FK_Attendance_CourseGroup FOREIGN KEY(course_group_id) REFERENCES CourseGroup(course_group_id)
 GO
 
 CREATE TABLE SysComment
@@ -195,7 +230,7 @@ CREATE TABLE SysComment
 	[update_time] DATETIME NULL
 )
 ALTER TABLE SysComment ADD CONSTRAINT PK_SysComment PRIMARY KEY CLUSTERED(comment_id) ON [PRIMARY]
-ALTER TABLE SysComment ADD CONSTRAINT FK_SysCommentUserCommenter FOREIGN KEY(commenter_id) REFERENCES SysUser(user_id)
+ALTER TABLE SysComment ADD CONSTRAINT FK_SysComment_UserCommenter FOREIGN KEY(commenter_id) REFERENCES SysUser(user_id)
 GO
 
 CREATE TABLE SysNotify
@@ -214,128 +249,54 @@ CREATE TABLE SysNotify
 	[update_time] DATETIME NULL
 )
 ALTER TABLE SysNotify ADD CONSTRAINT PK_SysNotify PRIMARY KEY CLUSTERED(notify_id) ON [PRIMARY]
-ALTER TABLE SysNotify ADD CONSTRAINT FK_SysNotifyUserSender FOREIGN KEY(sender_id) REFERENCES SysUser(user_id)
-ALTER TABLE SysNotify ADD CONSTRAINT FK_SysNotifyUserReceiver FOREIGN KEY(receiver_id) REFERENCES SysUser(user_id)
+ALTER TABLE SysNotify ADD CONSTRAINT FK_SysNotify_UserSender FOREIGN KEY(sender_id) REFERENCES SysUser(user_id)
+ALTER TABLE SysNotify ADD CONSTRAINT FK_SysNotify_UserReceiver FOREIGN KEY(receiver_id) REFERENCES SysUser(user_id)
 GO
 
--- Select Table
-SELECT *
-FROM SysRole
-SELECT *
-FROM SysUser
-
-SELECT *
-FROM Course
-SELECT *
-FROM Classroom
-SELECT *
-FROM Shift
-
-SELECT *
-FROM CourseGroup
-SELECT *
-FROM CourseGroupStudentList
-SELECT *
-FROM Schedule
-
-SELECT *
-FROM AttendanceRawData
-SELECT *
-FROM Attendance
-
-SELECT *
-FROM SysComment
-SELECT *
-FROM SysNotify
-
--- Chay dong nay nha Phuoc
-CREATE TABLE keyStore
+CREATE TABLE KeyStore
 (
-	[user_id] INT NOT NULL PRIMARY KEY,
+	[user_id] INT NOT NULL,
 	[privateKey] VARCHAR(256) NOT NULL,
 	[publicKey] VARCHAR(256) NOT NULL,
-	[refreshTokenUsing] VARCHAR(512) Unique NOT NULL,
+	[refreshTokenUsing] VARCHAR(512) UNIQUE NOT NULL,
 	[create_time] DATETIME NULL,
-	[update_time] DATETIME NULL,
-	CONSTRAINT FK_User FOREIGN KEY (user_id) REFERENCES SysUser(user_id)
+	[update_time] DATETIME NULL
 )
-
--- Update 2 nha Phuoc them khoa và khóa
-
-CREATE TABLE Faculty
-(
-	[faculty_id] INT NOT NULL PRIMARY KEY,
-	[faculty_name] NVARCHAR(256) NOT NULL,
-	[creator_id] INT NOT NULL,
-	[updater_id] INT NOT NULL,
-	[create_time] DATETIME NULL,
-	[update_time] DATETIME NULL,
-)
-
-ALTER TABLE SysUser
-ADD course_year INT NULL,
-    faculty_id INT NULL,
-	gender Bit Null;
-
-
-ALTER TABLE SysUser
-ADD CONSTRAINT FK_SysUser_Faculty FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id);
-
-ALTER TABLE Course
-ADD faculty_id INT NULL;
-
-ALTER TABLE Course
-ADD CONSTRAINT FK_Course_Faculty FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id);
-
-ALTER TABLE Faculty
-ADD CONSTRAINT FK_Faculty_SysUser FOREIGN KEY (creator_id) REFERENCES SysUser(user_id);
-
-ALTER TABLE Faculty
-ADD CONSTRAINT FK_Faculty_SysUser2 FOREIGN KEY (updater_id) REFERENCES SysUser(user_id);
-
---- 3 So cuoi dien thoai + Ten khong dau (Chu cai dau viet hoa) + 4 so cuoi mssv
-
---- Update new Create ClassRoomShift
-CREATE TABLE ClassRoomShift
-(
-	classroomshift_id INT IDENTITY(1, 1) NOT NULL,
-	classroom_code VARCHAR(32),
-	shift_code VARCHAR(32),
-	status BIT DEFAULT 0,
-	[creator_id] INT NULL,
-	[updater_id] INT NULL,
-	[create_time] DATETIME NULL,
-	[update_time] DATETIME NULL,
-	FOREIGN KEY (classroom_code) REFERENCES Classroom(classroom_code),
-	FOREIGN KEY (shift_code) REFERENCES Shift(shift_code)
-);
-ALTER TABLE ClassRoomShift ADD CONSTRAINT PK_ClassRoomShift PRIMARY KEY CLUSTERED(classroomshift_id) ON [PRIMARY]
-
--- Update 26/07
-ALTER TABLE CourseGroup
-ADD classroomshift_id INT UNIQUE
-
-ALTER TABLE CourseGroup
-ADD CONSTRAINT FK_CourseGroup_ClassRoomShift2 FOREIGN KEY (classroomshift_id) REFERENCES ClassRoomShift(classroomshift_id);
-
---- Update PostGroup - cac bài đăng trong nhóm
+ALTER TABLE KeyStore ADD CONSTRAINT PK_KeyStore PRIMARY KEY CLUSTERED(user_id) ON [PRIMARY]
+ALTER TABLE KeyStore ADD CONSTRAINT FK_KeyStore_User FOREIGN KEY(user_id) REFERENCES SysUser(user_id)
+GO
 
 CREATE TABLE PostGroup
 (
-	post_id INT IDENTITY(1, 1) NOT NULL,
-	course_group_id INT,
-	title NVARCHAR(256) NOT NULL,
-	content NVARCHAR(1024) NOT NULL,
-	file_link VARCHAR(256),
-	status BIT DEFAULT 0,
+	[post_id] INT IDENTITY(1, 1) NOT NULL,
+	[course_group_id] INT,
+	[title] NVARCHAR(256) NOT NULL,
+	[content] NVARCHAR(1024) NOT NULL,
+	[file_link] VARCHAR(256),
+	[status] BIT DEFAULT 0,
 	[creator_id] INT NULL,
 	[updater_id] INT NULL,
 	[create_time] DATETIME NULL,
-	[update_time] DATETIME NULL,
-	FOREIGN KEY (course_group_id) REFERENCES CourseGroup(course_group_id),
-
-);
+	[update_time] DATETIME NULL
+)
 ALTER TABLE PostGroup ADD CONSTRAINT PK_PostGroup PRIMARY KEY CLUSTERED(post_id) ON [PRIMARY]
+ALTER TABLE PostGroup ADD CONSTRAINT FK_PostGroup_CourseGroup FOREIGN KEY (course_group_id) REFERENCES CourseGroup(course_group_id);
+GO
 
-ALTER TABLE PostGroup
-ADD CONSTRAINT FK_PostGroup_CourseGroup FOREIGN KEY (course_group_id) REFERENCES CourseGroup(course_group_id);
+-- Select Table
+SELECT * FROM SysRole
+SELECT * FROM SysUser
+
+SELECT * FROM Course
+SELECT * FROM Classroom
+SELECT * FROM Shift
+
+SELECT * FROM CourseGroup
+SELECT * FROM CourseGroupStudentList
+SELECT * FROM Schedule
+
+SELECT * FROM AttendanceRawData
+SELECT * FROM Attendance
+
+SELECT * FROM SysComment
+SELECT * FROM SysNotify
--- sql/AttendanceSystemData.sql
+++ sql/AttendanceSystemData.sql
@@ -6,25 +6,19 @@ INSERT INTO SysRole(role_id, role_name, status, creator_id, updater_id, create_t
 	(3, N'Sinh viên', 1, 1, 1, '2024-05-17 20:42:33.567', '2024-05-17 20:42:33.567')
 
 SET IDENTITY_INSERT SysRole OFF
+GO
 
--- ID auto increase
--- Password admin: 123Admin456
--- password 3 số cuối sdt + Ten(inhoa chữ đầu) + khóa (Guang vien)
--- password 3 so cuối sdt + Ten(inhoa chữ đầu) + 4 số cuối MSSV (Sinh viên)
-
-INSERT INTO SysUser(email, username, password, nickname, phone, avatar_path, face_image_path, role_id, status, creator_id, updater_id, create_time, update_time) VALUES('admin@gmail.com', 'admin', '$2b$10$80tyEQ0RHJEVM6mBCNRJFOnFtn1Bm3TqHoBsYXTvnFLSV9FEGDNW2', N'Quản trị viên', '0123456789', '', '', 1, 1, 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567')
-
--- New 20/07 (update2)
-INSERT INTO Faculty(faculty_id, faculty_name, creator_id, updater_id, create_time, update_time) VALUES(10010, N'Công Nghệ Thông Tin', 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567')
-INSERT INTO Faculty(faculty_id, faculty_name, creator_id, updater_id, create_time, update_time) VALUES(10011, N'Quản Trị Kinh Doanh', 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567')
-INSERT INTO Faculty(faculty_id, faculty_name, creator_id, updater_id, create_time, update_time) VALUES(10012, N'Điện - Điện tử', 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567')
-
--- 24/07 Du lieu mon hoc
 --Them khoa
-INSERT INTO Faculty(faculty_id, faculty_name, creator_id, updater_id, create_time, update_time) VALUES(10013, N'Kế Toán', 1, 1, getdate(),getdate())
-INSERT INTO Faculty(faculty_id, faculty_name, creator_id, updater_id, create_time, update_time) VALUES(10014, N'Luật', 1, 1, getdate(),getdate())
-INSERT INTO Faculty(faculty_id, faculty_name, creator_id, updater_id, create_time, update_time) VALUES(10015, N'Thết kế đồ họa', 1, 1, getdate(),getdate())
+INSERT INTO Faculty(faculty_id, faculty_name, creator_id, updater_id, create_time, update_time) VALUES
+	(10010, N'Công Nghệ Thông Tin', 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(10011, N'Quản Trị Kinh Doanh', 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(10012, N'Điện - Điện tử', 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(10013, N'Kế Toán', 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(10014, N'Luật', 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(10015, N'Thết kế đồ họa', 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567')
+GO
 
+--Them mon hoc
 -- Khoa CNTT 10010
 INSERT INTO Course(course_code, course_name, credit, description, status, creator_id, create_time,faculty_id) VALUES
 ('1001002',N'Lập trình hướng đối tượng',4,N'Các chủ đề của học phần bao gồm: khái niệm đối tượng và mô hình hóa vấn đề hướng đối tượng, lớp và phương thức.',1,1,getdate(),10010),
@@ -39,6 +33,7 @@ INSERT INTO Course(course_code, course_name, credit, description, status, creato
 ('1001008',N'Công nghệ Java',3,N'Ngôn ngữ lập trình Java cơ bản và nâng cao. Làm việc với cơ sở dữ liệu trong Java: JDBC, Hibernate, Spring Data JPA. Các công nghệ và kỹ thuật trong phát triển phần mềm trên nền tảng Java như Servlet & Spring.',1,1,getdate(),10010),
 ('1001009',N'Mẫu thiết kế',3,N'Học phần này cũng giới thiệu cách các ngôn ngữ lập trình cấp cao (như Java, C#) sử dụng mẫu thiết kế để xây dựng các lớp đối tượng. Nhằm giúp sinh viên hiểu rõ hơn và vận dụng tốt hơn mẫu thiết kế vào công việc của mình.',1,1,getdate(),10010),
 ('1001010',N'Phát triển ứng dụng web với NodeJS',3,N'Các chủ đề của mô-đun bao gồm: Cách tạo trang web bằng NodeJS, HTML, CSS. Tăng tương tác với người dùng bằng ngôn ngữ Javascript.',1,1,getdate(),10010)
+GO
 
 -- Khoa QTKD 10011
 INSERT INTO Course(course_code, course_name, credit, description, status, creator_id, create_time,faculty_id) VALUES
@@ -52,6 +47,7 @@ INSERT INTO Course(course_code, course_name, credit, description, status, creato
 ('1001108',N'Tiếp thị B2B',3,N'This course is a study of the core concepts of marketing as applied to Business to Business (B2B) activity.',1,1,getdate(),10011),
 ('1001109',N'Quản trị bán lẻ',3,N'An overview of retailing from a management perspective. The development of retailing; the Australian retail industry and its environment; merchandising planning, control and distribution; pricing merchandise.',1,1,getdate(),10011),
 ('1001110',N'Tiếp thị công nghệ số',3,N'This course provides students a new view about marketing in the era of Internet. Beside introducing knowledge about digital marketing.',1,1,getdate(),10011)
+GO
 
 -- Khoa dien dien tu 10012
 INSERT INTO Course(course_code, course_name, credit, description, status, creator_id, create_time, faculty_id) VALUES
@@ -65,6 +61,7 @@ INSERT INTO Course(course_code, course_name, credit, description, status, creato
 ('1001208',N'Xử lý tín hiệu số',4,N'Xử lý tín hiệu số tập trung vào việc xử lý và phân tích tín hiệu số trong hệ thống điện tử.',1,1,getdate(),10012),
 ('1001209',N'Cấu trúc phần cứng máy tính',4,N'Cấu trúc phần cứng máy tính giúp sinh viên hiểu về các thành phần cấu tạo nên máy tính và hoạt động của chúng.',1,1,getdate(),10012),
 ('1001210',N'Kỹ thuật mạch điện tử',3,N'Kỹ thuật mạch điện tử tập trung vào việc thiết kế và phân tích mạch điện tử trong các hệ thống điện tử.',1,1,getdate(),10012);
+GO
 
 -- Khoa kế toán 10013
 INSERT INTO Course(course_code, course_name, credit, description, status, creator_id, create_time, faculty_id) VALUES
@@ -78,6 +75,8 @@ INSERT INTO Course(course_code, course_name, credit, description, status, creato
 ('1001308',N'Kế toán quốc tế 1',3,N'Kế toán quốc tế 1 giúp sinh viên hiểu về các tiêu chuẩn kế toán quốc tế và ứng dụng chúng trong thực tế.',1,1,getdate(),10013),
 ('1001309',N'Phân tích tài chính 1',3,N'Phân tích tài chính 1 tập trung vào việc phân tích báo cáo tài chính để đưa ra các quyết định chiến lược cho doanh nghiệp.',1,1,getdate(),10013),
 ('1001310',N'Kế toán hành chính sự nghiệp 1',3,N'Kế toán hành chính sự nghiệp 1 giúp sinh viên hiểu về công tác kế toán trong các cơ quan hành chính công.',1,1,getdate(),10013);
+GO
+
 -- Khoa Luật 10014
 INSERT INTO Course(course_code, course_name, credit, description, status, creator_id, create_time, faculty_id) VALUES
 ('1001401',N'Lý luận pháp luật',3,N'Lý luận pháp luật là môn học cung cấp kiến thức về các nguyên lý cơ bản và hệ thống pháp luật.',1,1,getdate(),10014),
@@ -90,6 +89,8 @@ INSERT INTO Course(course_code, course_name, credit, description, status, creato
 ('1001408',N'Luật quốc tế 1',3,N'Luật quốc tế 1 giúp sinh viên hiểu về hệ thống pháp luật quốc tế và quan hệ pháp lý giữa các quốc gia.',1,1,getdate(),10014),
 ('1001409',N'Luật sự nghiệp 1',3,N'Luật sự nghiệp 1 giúp sinh viên hiểu về quy trình và công việc của một luật sư trong thực tế.',1,1,getdate(),10014),
 ('1001410',N'Luật thông tin 1',3,N'Luật thông tin 1 tập trung vào việc nghiên cứu về quy định pháp lý liên quan đến thông tin và truyền thông.',1,1,getdate(),10014);
+GO
+
 -- Khoa Thiết kế đồ họa 10015
 INSERT INTO Course(course_code, course_name, credit, description, status, creator_id, create_time, faculty_id) VALUES
 ('1001501', N'Đồ họa Vector', 3, N'Môn học Đồ họa Vector giúp sinh viên nắm vững về các kỹ thuật thiết kế vector.', 1, 1, GETDATE(), 10015),
@@ -102,6 +103,7 @@ INSERT INTO Course(course_code, course_name, credit, description, status, creato
 ('1001508', N'Đồ họa số', 2, N'Môn học Đồ họa số giúp sinh viên hiểu rõ về các khái niệm và công nghệ trong đồ họa số.', 1, 1, GETDATE(), 10015),
 ('1001509', N'Thiết kế đồ họa đa phương tiện', 3, N'Môn học Thiết kế đồ họa đa phương tiện tập trung vào việc phát triển kỹ năng thiết kế đồ họa đa phương tiện.', 1, 1, GETDATE(), 10015),
 ('1001510', N'Đồ họa và truyền thông', 3, N'Đồ họa và truyền thông là môn học giúp sinh viên hiểu về tầm quan trọng của đồ họa trong truyền thông.', 1, 1, GETDATE(), 10015);
+GO
 
 ---- Update du lieu Shift
 insert into Shift(shift_code,start_time,end_time,break_hour,status,creator_id,create_time) VALUES
@@ -110,5 +112,71 @@ insert into Shift(shift_code,start_time,end_time,break_hour,status,creator_id,cr
 ('ca3','12h45','15h15','15',1,1,getDate()),
 ('ca4','15h25','17h55','15',1,1,getDate()),
 ('ca5','18h05','20h35','15',1,1,getDate())
+GO
 
---
+-- Dữ liệu phòng học
+INSERT INTO Classroom(classroom_code, capacity, floor, description, status, creator_id, updater_id, create_time, update_time) VALUES
+	('C201', 50, 2, N'Phòng học có máy chiếu', 1, 1, 1, '2024-07-28 18:42:33.567', '2024-07-28 18:42:33.567'),
+	('C202', 50, 2, N'Phòng học có máy chiếu', 1, 1, 1, '2024-07-28 18:42:33.567', '2024-07-28 18:42:33.567'),
+	('C203', 50, 2, N'Phòng học có máy chiếu', 1, 1, 1, '2024-07-28 18:42:33.567', '2024-07-28 18:42:33.567'),
+	('C204', 50, 2, N'Phòng học có máy chiếu', 1, 1, 1, '2024-07-28 18:42:33.567', '2024-07-28 18:42:33.567'),
+	('C205', 50, 2, N'Phòng học có máy chiếu', 1, 1, 1, '2024-07-28 18:42:33.567', '2024-07-28 18:42:33.567')
+GO
+
+-- ID auto increase
+-- Password admin: 123456
+-- password 3 số cuối sdt + Ten(inhoa chữ đầu) + khóa (Guang vien)
+-- password 3 so cuối sdt + Ten(inhoa chữ đầu) + 4 số cuối MSSV (Sinh viên)
+
+INSERT INTO SysUser(email, username, password, nickname, phone, avatar_path, face_image_path, role_id, status, creator_id, updater_id, create_time, update_time) VALUES
+	('admin@gmail.com', 'admin', '$2b$10$80tyEQ0RHJEVM6mBCNRJFOnFtn1Bm3TqHoBsYXTvnFLSV9FEGDNW2', N'Quản trị viên', '0123456789', '', '', 1, 1, 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567')
+
+-- Dữ liệu người dùng ban đầu tạo để test.
+SET IDENTITY_INSERT SysUser ON
+INSERT INTO SysUser(user_id, email, username, password, nickname, phone, avatar_path, face_image_path, role_id, status, creator_id, updater_id, create_time, update_time) VALUES
+	(899, 'tranvana@gmail.com', 'admin', '$2b$10$80tyEQ0RHJEVM6mBCNRJFOnFtn1Bm3TqHoBsYXTvnFLSV9FEGDNW2', N'Trần Văn A', '0123456789', '', '', 2, 1, 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(900, '52000900@gmail.com', '52000900', '$2b$10$80tyEQ0RHJEVM6mBCNRJFOnFtn1Bm3TqHoBsYXTvnFLSV9FEGDNW2', N'Nguyễn Ðức Quyền', '0123456789', '', '', 3, 1, 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(901, '52000901@gmail.com', '52000901', '$2b$10$80tyEQ0RHJEVM6mBCNRJFOnFtn1Bm3TqHoBsYXTvnFLSV9FEGDNW2', N'Mã Công Hoán', '0123456789', '', '', 3, 1, 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(902, '52000902@gmail.com', '52000902', '$2b$10$80tyEQ0RHJEVM6mBCNRJFOnFtn1Bm3TqHoBsYXTvnFLSV9FEGDNW2', N'Hoàng Chiêu Phong', '0123456789', '', '', 3, 1, 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(903, '52000903@gmail.com', '52000903', '$2b$10$80tyEQ0RHJEVM6mBCNRJFOnFtn1Bm3TqHoBsYXTvnFLSV9FEGDNW2', N'Thái Hồng Thư', '0123456789', '', '', 3, 1, 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(904, '52000904@gmail.com', '52000904', '$2b$10$80tyEQ0RHJEVM6mBCNRJFOnFtn1Bm3TqHoBsYXTvnFLSV9FEGDNW2', N'Vũ Lệ Khánh', '0123456789', '', '', 3, 1, 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567'),
+	(905, '52000905@gmail.com', '52000905', '$2b$10$80tyEQ0RHJEVM6mBCNRJFOnFtn1Bm3TqHoBsYXTvnFLSV9FEGDNW2', N'Đỗ Ái Hồng', '0123456789', '', '', 3, 1, 1, 1, '2024-05-26 12:42:33.567', '2024-05-26 12:42:33.567')
+SET IDENTITY_INSERT SysUser OFF
+GO
+
+-- Dữ liệu thời gian ca học
+SET IDENTITY_INSERT ClassRoomShift ON
+INSERT INTO ClassRoomShift(classroomshift_id, classroom_code, shift_code, status, creator_id, updater_id, create_time, update_time) VALUES
+	(201, 'C201', 'ca1', 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230'),
+	(202, 'C202', 'ca1', 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230'),
+	(203, 'C203', 'ca1', 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230'),
+	(204, 'C204', 'ca1', 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230')
+SET IDENTITY_INSERT ClassRoomShift OFF
+GO
+
+-- Dữ liệu nhóm môn học
+SET IDENTITY_INSERT CourseGroup ON
+INSERT INTO CourseGroup(course_group_id, course_code, group_code, teacher_id, classroomshift_id, total_student_qty, status, creator_id, updater_id, create_time, update_time) VALUES
+	(100, '1001002', 'N1', 899, 201, 6, 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230')
+SET IDENTITY_INSERT CourseGroup OFF
+GO
+
+-- Dữ liệu danh sách sinh viên nhóm môn học
+INSERT INTO CourseGroupStudentList(course_group_id, student_id, total_absent, ban_yn, status, creator_id, updater_id, create_time, update_time) VALUES
+	(100, 900, 0, 0, 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230'),
+	(100, 901, 0, 0, 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230'),
+	(100, 902, 0, 0, 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230'),
+	(100, 903, 0, 0, 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230'),
+	(100, 904, 0, 0, 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230'),
+	(100, 905, 0, 0, 1, 1, 1, '2024-07-28 18:11:02.230', '2024-07-28 18:11:02.230')
+GO
+
+-- Dữ liệu điểm danh để test
+INSERT INTO AttendanceRawData(student_id, course_group_id, attend_date, attend_type, attend_time, attend_image_path, creator_id, create_time) VALUES
+	(900, 100, '20240728', 0, '06:30', '', 899, '2024-07-28 18:11:02.230'),
+	(901, 100, '20240728', 0, '06:30', '', 899, '2024-07-28 18:11:02.230'),
+	(902, 100, '20240728', 0, '06:30', '', 899, '2024-07-28 18:11:02.230'),
+	(903, 100, '20240728', 0, '06:30', '', 899, '2024-07-28 18:11:02.230'),
+	(904, 100, '20240728', 0, '06:30', '', 899, '2024-07-28 18:11:02.230'),
+	(905, 100, '20240728', 0, '06:30', '', 899, '2024-07-28 18:11:02.230')
+GO
